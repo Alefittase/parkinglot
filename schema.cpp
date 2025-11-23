33765b6ac5f1dcc771ec51b636d4ed12c1a1d587
Ali Nasrollahour <alefittase@Alis-MacBook-Pro.local>
2025-11-23 11:10:11 +0530

    created a basic schema for the project

---
+++ schema.cpp
@@ -0,0 +1,20 @@
+UI input -> enterance queue -> first empty stack (if all are full, return "parking is full")
+
+parking:
+    n stacks // based on linked list
+    each stack has m nodes at most
+        each node is a car:
+            id, type, ...
+
+
+functions:
+    insert(car) // adds car to first not-full stack
+    insertAt(car, i) // adds car to ith stack
+    sort(i) // sorts cars in ith stack based on id
+    move(i, j) //moves as many cars as possible from ith stack to jth stack
+               //if j gets full, continue to j+1, j+2, ...
+    
+
+notes:
+    dont use Stack/Queue/LinkedList libraries
+    
